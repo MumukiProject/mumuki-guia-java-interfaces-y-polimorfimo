8c950ccbc1d0af1b47ffe305bd15a928ad4d3b81
franco@mumuki.org <franco@mumuki.org>
2017-08-15 15:46:14 -0400

    Mumuki Export on 2017-08-15 15:46:14 -0400

--- 00007_NullObject karatecas/test.java
+++
@@ -1,9 +0,0 @@
-@Test
-public void no_refuerza_el_ataque() {
-  Assert.assertEquals(0, new ManoDesnuda().refuerzoOfensivo());
-}
-
-@Test
-public void no_refuerza_la_defensa() {
-  Assert.assertEquals(, new ManoDesnuda().refuerzoDefensivo());
-}

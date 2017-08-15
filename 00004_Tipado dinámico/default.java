3dfd01b00f58632dcd52b894592564afda9f22d5
franco@mumuki.org <franco@mumuki.org>
2017-08-15 13:03:45 -0400

    Mumuki Export on 2017-08-15 13:03:45 -0400

---
+++ 00004_Tipado dinámico/default.java
@@ -0,0 +1,24 @@
+class Linterna 
+  def iluminar!
+    # ...ilumina como un haz de luz potente...
+  end
+end
+
+class Vela 
+  def alumbrar!
+    # ...ilumina con un resplandor débil...
+  end
+end
+
+class Cueva
+  def entrar_con!(iluminador) 
+    iluminador.iluminar!
+    proyectar_sombras_terrorificas!
+  end
+  def proyectar_sombras_terrorificas!
+    #...
+  end
+end
+
+la_cueva = Cueva.new 
+la_cueva.entrar_con! Vela.new

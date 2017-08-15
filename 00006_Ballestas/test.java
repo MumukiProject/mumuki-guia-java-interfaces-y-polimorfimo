54f3385c55a4b403b9a3ffec758e459fe16bc7e2
franco@mumuki.org <franco@mumuki.org>
2017-08-15 14:48:39 -0400

    Mumuki Export on 2017-08-15 14:48:39 -0400

--- 00006_Ballestas/test.java
+++ 00006_Ballestas/test.java
@@ -1,1 +1,9 @@
-TODO
+@Test
+public void una_ballesta_refuerza_en_8_el_ataque() {
+  Assert.assertEquals(8, new Ballesta().refuerzoOfensivo());
+}
+
+@Test
+public void una_ballesta_no_refuerza_la_defensa() {
+  Assert.assertEquals(0, new Ballesta().refuerzoDefensivo());
+}
